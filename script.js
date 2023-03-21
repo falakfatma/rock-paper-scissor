68a1bcfb4599d8be2321629720483e85d42c7fe5
Falak Fatma <102820083+falakfatma@users.noreply.github.com>
2023-03-21 17:40:45 +0000

    layout of this game

---
+++ script.js
@@ -0,0 +1,22 @@
+// let container = document.querySelector(`.container`)
+// let gamingItem = document.querySelectorAll(`.gaming-box`)
+// let ele = document.createElement('div')
+// ele.classList = "turn"
+// let turn = `Turn : Your turn`
+// ele.innerHTML = turn
+// container.append(ele)
+
+// const Which_turn = () => {
+//   if (turn = `Your turn`) {
+//     turn = `computer`
+//   }
+//   else {
+//     turn = `Your turn`
+//   }
+
+// }
+// console.log(gamingItem)
+// let item = gamingItem.forEach((e)=>{
+// return e
+// })
+// console.log(item)
